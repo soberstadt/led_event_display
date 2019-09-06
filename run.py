98307d90cdb72ef606836c299f723e5ab850b661
Spencer Oberstadt <soberstadt@gmail.com>
2019-09-05 22:23:11 -0400

    use env vars for settings

--- run.py
+++ run.py
@@ -1,20 +1,27 @@
 import time
+import os
 from rpi_ws281x import PixelStrip, Color
 import argparse
 import random
 import requests
 
 # LED strip configuration:
-LED_COUNT = 300       # Number of LED pixels.
-LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
-# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
-LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA = 10          # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
-LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+# Number of LED pixels.
+LED_COUNT = int(os.environ.get('LED_COUNT') or 300)
+# GPIO pin connected to the pixels (18 uses PWM!).
+LED_PIN = int(os.environ.get('LED_PIN') or 18)
+# LED signal frequency in hertz (usually 800khz)
+LED_FREQ_HZ = int(os.environ.get('LED_FREQ_HZ') or 800000)
+# DMA channel to use for generating signal (try 10)
+LED_DMA = int(os.environ.get('LED_DMA') or 10)
+# Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = int(os.environ.get('LED_BRIGHTNESS') or 255)
+# True to invert the signal (when using NPN transistor level shift)
+LED_INVERT = False
+# set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_CHANNEL = 0
 
-TIME_INTERVAL = 5000
+TIME_INTERVAL = int(os.environ.get('TIME_INTERVAL') or 5000)
 
 start = int(round(time.time() * 1000))
 
