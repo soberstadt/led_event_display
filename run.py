a798516fca0df1ffffb2ddafe1ce73cbd2af44d9
Spencer Oberstadt <soberstadt@gmail.com>
2019-09-09 15:43:01 -0400

    clean up code some

--- run.py
+++ run.py
@@ -32,6 +32,56 @@ start = int(round(time.time() * 1000))
 next_color = Color(255, 0, 0)
 events = {}
 
+color_map = {
+    'www.everyarabstudent.com-web': Color(255, 0, 0),
+    'www.mirstudentov.com-web': Color(255, 153, 0),
+    'www.suaescolha.com-web': Color(204, 255, 0),
+    'www.mahasiswakeren.com-web': Color(51, 255, 0),
+    'www.kazdystudent.pl-web': Color(0, 255, 102),
+    'www.everystudent.com-web': Color(0, 255, 255),
+    'www.cadaestudiante.com-web': Color(0, 102, 255),
+    'www.kampusweb.com-web': Color(51, 0, 255),
+    'www.everystudent.hu-web': Color(204, 0, 255),
+    'www.everystudent.ro-web': Color(255, 0, 153)
+}
+
+es_prc_links = [
+    "https://www.everystudent.com/features/followup.html",
+    "https://www.cadaestudiante.com/articulos/conociendo2.html",
+    "https://www.suaescolha.com/pessoalmente2/",
+    "https://www.kampusweb.com/konular/kabul.html",
+    "https://www.mahasiswakeren.com/artikel/402mengenal.html",
+    "https://www.kazdystudent.pl/a/nowezycie.html",
+    "https://www.everyarabstudent.com/a/fol.html",
+    "https://www.everystudent.ro/a/interesa.html",
+    "https://www.everystudent.hu/a/hogyantovabb.html",
+    "https://www.mirstudentov.com/a/fol.html"
+]
+
+es_query_payload = {
+    "size" : 300,
+    "_source": ["collector_tstamp", "app_id", "unstruct_event_com_snowplowanalytics_snowplow_link_click_1.targetUrl"],
+    "query": {
+        "bool": {
+            "should": [],
+            "minimum_should_match" : 1,
+            "must": {
+                "range": {
+                    "collector_tstamp": { "gte" : "now-4h" }
+                }
+            }
+        }
+    }
+}
+for es_url in es_prc_links:
+    es_query_payload['query']['bool']['should'].append(
+        {
+            "term": {
+                "unstruct_event_com_snowplowanalytics_snowplow_link_click_1.targetUrl": es_url
+            }
+        }
+    )
+
 def push_pixels(strip):
     tick = TIME_INTERVAL / 1000
     now = int(datetime.datetime.utcnow().strftime("%s"))
@@ -41,96 +91,26 @@ def push_pixels(strip):
         higher = now - (tick * i)
         app_id = None
         for date, app in events.items():
-            if (date < higher) and (date > lower):
+            if (date < higher) and (date >= lower):
                 print('found %s', app)
                 app_id = app
                 break
-        color = 0
-        if app_id != None:
-            print('making pixel red: %i', i)
-            color = Color(255, 0, 0)
-        strip.setPixelColor(i, color)
+        strip.setPixelColor(i, color_for(app_id))
 
     strip.show()
 
-def add_pixel(strip):
-    global next_color
-    if next_color != None:
-        strip.setPixelColor(0, next_color)
-        next_color = None
-        strip.show()
+def color_for(app_id):
+    if app_id == None:
+        return 0
+    if app_id in color_map:
+        return color_map[app_id]
+    return Color(255, 255, 255)
 
 def get_es_data():
     auth_token = os.environ.get('AUTH_TOKEN')
     url = 'https://org-cru-prod1.elasticsearch.snplow.net/_plugin/kibana/api/console/proxy?path=_search&method=POST'
     headers = { 'kbn-version': '6.5.4', 'authorization': 'Basic ' + auth_token, 'content-type': 'application/json' }
-    payload = {
-        "size" : 300,
-        "_source": ["collector_tstamp", "app_id", "unstruct_event_com_snowplowanalytics_snowplow_link_click_1.targetUrl"],
-        "query": {
-            "bool": {
-                "should": [
-                    {
-                        "term": {
-                            "unstruct_event_com_snowplowanalytics_snowplow_link_click_1.targetUrl": "https://www.everystudent.com/features/followup.html"
-                        }
-                    },
-                    {
-                        "term": {
-                            "unstruct_event_com_snowplowanalytics_snowplow_link_click_1.targetUrl": "https://www.cadaestudiante.com/articulos/conociendo2.html"
-                        }
-                    },
-                    {
-                        "term": {
-                            "unstruct_event_com_snowplowanalytics_snowplow_link_click_1.targetUrl": "https://www.suaescolha.com/pessoalmente2/"
-                        }
-                    },
-                    {
-                        "term": {
-                            "unstruct_event_com_snowplowanalytics_snowplow_link_click_1.targetUrl": "https://www.kampusweb.com/konular/kabul.html"
-                        }
-                    },
-                    {
-                        "term": {
-                            "unstruct_event_com_snowplowanalytics_snowplow_link_click_1.targetUrl": "https://www.mahasiswakeren.com/artikel/402mengenal.html"
-                        }
-                    },
-                    {
-                        "term": {
-                            "unstruct_event_com_snowplowanalytics_snowplow_link_click_1.targetUrl": "https://www.kazdystudent.pl/a/nowezycie.html"
-                        }
-                    },
-                    {
-                        "term": {
-                            "unstruct_event_com_snowplowanalytics_snowplow_link_click_1.targetUrl": "https://www.everyarabstudent.com/a/fol.html"
-                        }
-                    },
-                    {
-                        "term": {
-                            "unstruct_event_com_snowplowanalytics_snowplow_link_click_1.targetUrl": "https://www.everystudent.ro/a/interesa.html"
-                        }
-                    },
-                    {
-                        "term": {
-                            "unstruct_event_com_snowplowanalytics_snowplow_link_click_1.targetUrl": "https://www.everystudent.hu/a/hogyantovabb.html"
-                        }
-                    },
-                    {
-                        "term": {
-                            "unstruct_event_com_snowplowanalytics_snowplow_link_click_1.targetUrl": "https://www.mirstudentov.com/a/fol.html"
-                        }
-                    }
-                ],
-                "minimum_should_match" : 1,
-                "must": {
-                    "range": {
-                        "collector_tstamp": { "gte" : "now-4h" }
-                    }
-                }
-            }
-        }
-    }
-    r = requests.post(url, headers=headers, data=json.dumps(payload))
+    r = requests.post(url, headers=headers, data=json.dumps(es_query_payload))
     return r.json()
 
 def sync_data():
@@ -177,7 +157,6 @@ if __name__ == '__main__':
         while True:
             sync_data()
             push_pixels(strip)
-            # add_pixel(strip)
             wait()
 
     except KeyboardInterrupt:
